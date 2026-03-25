72bf85c43b73fcc2c95136154944f860cac47790
virendrathakre03 <virendrathakre03@gmail.com>
2026-03-25 08:58:53 +0530

    end to end pipeline with experiment tracking by dvclive

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,8 +5,8 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
-# import yaml
-# from dvclive import Live
+import yaml
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -30,22 +30,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-# def load_params(params_path: str) -> dict:
-#     """Load parameters from a YAML file."""
-#     try:
-#         with open(params_path, 'r') as file:
-#             params = yaml.safe_load(file)
-#         logger.debug('Parameters retrieved from %s', params_path)
-#         return params
-#     except FileNotFoundError:
-#         logger.error('File not found: %s', params_path)
-#         raise
-#     except yaml.YAMLError as e:
-#         logger.error('YAML error: %s', e)
-#         raise
-#     except Exception as e:
-#         logger.error('Unexpected error: %s', e)
-#         raise
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def load_model(file_path: str):
     """Load the trained model from a file."""
@@ -74,8 +74,30 @@ def load_data(file_path: str) -> pd.DataFrame:
         logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
-def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
-    """Evaluate the model and return the evaluation metrics."""
+# def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
+#     """Evaluate the model and return the evaluation metrics."""
+#     try:
+#         y_pred = clf.predict(X_test)
+#         y_pred_proba = clf.predict_proba(X_test)[:, 1]
+
+#         accuracy = accuracy_score(y_test, y_pred)
+#         precision = precision_score(y_test, y_pred)
+#         recall = recall_score(y_test, y_pred)
+#         auc = roc_auc_score(y_test, y_pred_proba)
+
+#         metrics_dict = {
+#             'accuracy': accuracy,
+#             'precision': precision,
+#             'recall': recall,
+#             'auc': auc
+#         }
+#         logger.debug('Model evaluation metrics calculated')
+#         return metrics_dict
+#     except Exception as e:
+#         logger.error('Error during model evaluation: %s', e)
+#         raise
+
+def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray):
     try:
         y_pred = clf.predict(X_test)
         y_pred_proba = clf.predict_proba(X_test)[:, 1]
@@ -91,8 +113,10 @@ def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
             'recall': recall,
             'auc': auc
         }
+
         logger.debug('Model evaluation metrics calculated')
-        return metrics_dict
+        return metrics_dict, y_pred   # 👈 return y_pred also
+
     except Exception as e:
         logger.error('Error during model evaluation: %s', e)
         raise
@@ -112,16 +136,24 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
-
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
         X_test = test_data.iloc[:, :-1].values
         y_test = test_data.iloc[:, -1].values
 
-        metrics = evaluate_model(clf, X_test, y_test)
+        # metrics = evaluate_model(clf, X_test, y_test)
+        metrics, y_pred = evaluate_model(clf, X_test, y_test)
+        # Experiment tracking using dvclive
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy',accuracy_score(y_test,y_pred))
+            live.log_metric('precision',precision_score(y_test,y_pred))
+            live.log_metric('recall',recall_score(y_test,y_pred))
 
-        
+            live.log_params(params)
+
+
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
